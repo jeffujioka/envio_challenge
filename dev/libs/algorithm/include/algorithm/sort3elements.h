5d228e4de3e5449590e02d59be3296868bc1e0ac
Jefferson M Fujioka <jefferson.fujioka@gmail.com>
2021-01-20 22:10:11 +0100

    docs: add code documentation for Sort3Elements::Sort

--- dev/libs/algorithm/include/algorithm/sort3elements.h
+++ dev/libs/algorithm/include/algorithm/sort3elements.h
@@ -25,6 +25,16 @@ public:
   /// Explicitly Default-Move-Assignable
   Sort3Elements& operator=(Sort3Elements&& msg) = default;
 
+  /// \brief   Sorts an array of 0s, 1s and 2s in linear time complexity.
+  /// \details This implementation sorts the \p vec array in-place so the
+  ///          space complexity is constant (1).
+  ///          Example 1: 
+  ///            given array:            {1, 2, 0}
+  ///            the expected result is: {0, 1, 2}
+  ///          Example 2:
+  ///            given array:            {0, 1, 1, 2, 2, 0, 0, 1, 2, 1}
+  ///            the expected result is: {0, 0, 0, 1, 1, 1, 1, 2, 2, 2}
+  /// \param vec[in,out] The given to be sorted.
   void Sort(std::vector<uint32_t>& vec);
 };
 
